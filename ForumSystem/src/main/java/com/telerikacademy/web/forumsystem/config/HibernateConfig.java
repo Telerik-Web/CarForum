ccac687f9c3e37a942b2cd8291a9284c9c6bdced
Lachezar Lazarov <latschezar04@gmail.com>
2025-01-24 18:40:11 +0200

    Implement FilterPostOptions

--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/config/HibernateConfig.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/config/HibernateConfig.java
@@ -3,11 +3,9 @@ package com.telerikacademy.web.forumsystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
---
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/models/FilterPostOptions.java
@@ -0,0 +1,46 @@
+package com.telerikacademy.web.forumsystem.models;
+
+import java.util.Optional;
+
+public class FilterPostOptions {
+
+    private Optional<String> title;
+    private Optional<String> content;
+    private Optional<String> createdBy;
+    private Optional<String> sortBy;
+    private Optional<String> sortOrder;
+
+    public FilterPostOptions(
+            String title,
+            String content,
+            String createdBy,
+            String sortBy,
+            String sortOrder   ) {
+
+        this.title = Optional.ofNullable(title);
+        this.content = Optional.ofNullable(content);
+        this.createdBy = Optional.ofNullable(createdBy);
+        this.sortBy = Optional.ofNullable(sortBy);
+        this.sortOrder = Optional.ofNullable(sortOrder);
+    }
+
+    public Optional<String> getTitle() {
+        return title;
+    }
+
+    public Optional<String> getContent() {
+        return content;
+    }
+
+    public Optional<String> getCreatedBy() {
+        return createdBy;
+    }
+
+    public Optional<String> getSortBy() {
+        return sortBy;
+    }
+
+    public Optional<String> getSortOrder() {
+        return sortOrder;
+    }
+}
