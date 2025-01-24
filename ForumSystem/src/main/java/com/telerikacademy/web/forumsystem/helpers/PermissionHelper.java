6263eb50603b7c1f64f441ee4640460b73dfbb2f
Lachezar Lazarov <latschezar04@gmail.com>
2025-01-24 19:53:39 +0200

    Implement PermissionHelper

---
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/helpers/PermissionHelper.java
@@ -0,0 +1,35 @@
+package com.telerikacademy.web.forumsystem.helpers;
+
+import com.telerikacademy.web.forumsystem.exceptions.UnauthorizedOperationException;
+import com.telerikacademy.web.forumsystem.models.Post;
+import com.telerikacademy.web.forumsystem.models.User;
+
+public class PermissionHelper {
+    private static final String AUTHORIZATION_PERMISSION_ERROR = "You don't have the permission to do this.";
+    private static final String BLOCKED_USER_ERROR = "You are blocked and cannot perform that operation";
+
+    public static void checkIfCreator(User user, User userToUpdate) {
+        if (!(user.getId() == userToUpdate.getId())) {
+            throw new UnauthorizedOperationException(AUTHORIZATION_PERMISSION_ERROR);
+        }
+    }
+
+    public static void checkIfAdmin(User user) {
+        if (!user.isAdmin()) {
+            throw new UnauthorizedOperationException(AUTHORIZATION_PERMISSION_ERROR);
+        }
+    }
+
+    public static void checkIfCreatorOrAdmin(User user, Post post) {
+        if (!(user.equals(post.getCreatedBy()) || user.isAdmin())) {
+            throw new UnauthorizedOperationException(AUTHORIZATION_PERMISSION_ERROR);
+        }
+    }
+
+
+    public static void checkIfBlocked(User user) {
+        if (user.isBlocked()) {
+            throw new UnauthorizedOperationException(BLOCKED_USER_ERROR);
+        }
+    }
+}
