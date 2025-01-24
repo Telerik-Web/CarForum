a2f6a3b4f638633f7faf4ec7a35f0f27f757ed3a
Lachezar Lazarov <latschezar04@gmail.com>
2025-01-24 19:03:17 +0200

    Update AuthorizationHelper

--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/AuthorizationHelper.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/AuthorizationHelper.java
@@ -1,5 +1,6 @@
 package com.telerikacademy.web.forumsystem.controllers;
 
+import com.telerikacademy.web.forumsystem.exceptions.AuthorizationException;
 import com.telerikacademy.web.forumsystem.exceptions.UnauthorizedOperationException;
 import com.telerikacademy.web.forumsystem.models.User;
 import com.telerikacademy.web.forumsystem.services.UserService;
@@ -14,6 +15,9 @@ import org.springframework.web.server.ResponseStatusException;
 public class AuthorizationHelper {
 
     private static final String AUTHORIZATION = "Authorization";
+    public static final String AUTHENTICATION_ERROR = "The requested resource requires authentication.";
+    public static final String INVALID_AUTHENTICATION_ERROR = "Invalid authentication";
+
     private final UserService userService;
 
     @Autowired
@@ -23,15 +27,45 @@ public class AuthorizationHelper {
 
     public User tryGetUser(HttpHeaders headers) {
         if (!headers.containsKey(AUTHORIZATION)) {
-            throw new UnauthorizedOperationException("You are not authorized to perform this action");
+            throw new AuthorizationException(AUTHENTICATION_ERROR);
         }
 
-        String username = headers.getFirst(AUTHORIZATION);
+        String userInfo = headers.getFirst(AUTHORIZATION);
+        String username = getUsername(userInfo);
+        String password = getPassword(userInfo);
 
         try {
-            return userService.findByUsername(username);
+
+            User user = userService.findByUsername(username);
+
+            if(!user.getPassword().equals(password)){
+                throw new UnauthorizedOperationException(INVALID_AUTHENTICATION_ERROR);
+            }
+
+            return user;
+
         } catch (EntityNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+            throw new UnauthorizedOperationException(INVALID_AUTHENTICATION_ERROR);
         }
+    }
+
+    private String getUsername(String userInfo) {
+        int firstSpace = userInfo.indexOf(" ");
+
+        if (firstSpace == -1) {
+            throw new AuthorizationException(AUTHENTICATION_ERROR);
+        }
+        return userInfo.substring(0, firstSpace);
+
+    }
+
+    private String getPassword(String userInfo) {
+        int firstSpace = userInfo.indexOf(" ");
+
+        if (firstSpace == -1) {
+            throw new AuthorizationException(AUTHENTICATION_ERROR);
+        }
+        return userInfo.substring(firstSpace + 1);
+
     }
 }
