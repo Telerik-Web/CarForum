b6f5ca895301e3bc0ecb7c7bb268226c0edd22f8
Lachezar Lazarov <latschezar04@gmail.com>
2025-02-18 23:23:48 +0200

    Add most recent posts to HomeView

--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/HomeMvcController.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/HomeMvcController.java
@@ -1,6 +1,8 @@
 package com.telerikacademy.web.forumsystem.controllers.mvc;
 
+import com.telerikacademy.web.forumsystem.services.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeMvcController {
 
+    private final PostService postService;
+
+    public HomeMvcController(PostService postService) {
+        this.postService = postService;
+    }
+
     @GetMapping
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        model.addAttribute("mostRecentPosts", postService.getMostRecentPosts());
+        model.addAttribute("mostCommentedPosts", postService.getMostCommentedPosts());
         return "HomeView";
     }
 }
