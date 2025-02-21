43c711d58bcd60c4126b8f6e25e0647b60869f57
ivan200126y <ivan29654@gmail.com>
2025-02-21 12:02:27 +0200

    Implemeting Account page

--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/AuthenticationMvcController.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/AuthenticationMvcController.java
@@ -16,10 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/auth")
@@ -104,5 +101,22 @@ public class AuthenticationMvcController {
                     "Username is already taken!");
             return "Register";
         }
+    }
+
+
+
+    @GetMapping("/account")
+    public String showAccountPage(Model model,
+                                  HttpSession session) {
+        User user = authenticationHelper.tryGetUser(session);
+        model.addAttribute("user", user);
+        return "Account";
+    }
+
+    @GetMapping("/account/update/{id}")
+    public String showUserUpdateForm(@PathVariable int id,
+                                     Model model,
+                                     HttpSession session) {
+        return "UpdateUser";
     }
 }
--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/HomeMvcController.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/HomeMvcController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -62,8 +63,4 @@ public class HomeMvcController {
         return "About";
     }
 
-    @GetMapping("/account")
-    public String showAccountPage() {
-        return "Account";
-    }
 }
