316e60f212189fd1331af7ffce7dd11dcc52bdfc
ivan200126y <ivan29654@gmail.com>
2025-02-21 14:48:57 +0200

    uncomment code

--- ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/GlobalExceptionHandler.java
+++ ForumSystem/src/main/java/com/telerikacademy/web/forumsystem/controllers/mvc/GlobalExceptionHandler.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(Exception.class)
-//    public String handleTypeMismatchException(Exception ex, Model model) {
-//        model.addAttribute("errorMessage", ex.getMessage());
-//        return "NotFound";
-//    }
+    @ExceptionHandler(Exception.class)
+    public String handleTypeMismatchException(Exception ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "NotFound";
+    }
 
 }
